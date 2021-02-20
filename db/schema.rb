948899905090a741d320f30c15c0e7551cd5fca0
muranokami <pikupiku68574.dj@gmail.com>
2021-02-20 17:24:15 +0900

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 2021_02_20_070925) do
+=======
+ActiveRecord::Schema.define(version: 2021_02_20_075120) do
+>>>>>>> Stashed changes
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -28,6 +32,10 @@ ActiveRecord::Schema.define(version: 2021_02_20_070925) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+<<<<<<< Updated upstream
+=======
+    t.string "nickname"
+>>>>>>> Stashed changes
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
