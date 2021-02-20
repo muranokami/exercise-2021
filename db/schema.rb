668b5e1370c0b8841dff8e68fb914de422fd4a9a
muranokami <pikupiku68574.dj@gmail.com>
2021-02-20 16:41:42 +0900

    commit3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_082540) do
+ActiveRecord::Schema.define(version: 2021_02_20_070925) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -21,10 +21,6 @@ ActiveRecord::Schema.define(version: 2021_02_18_082540) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "profile", null: false
-    t.text "occupation", null: false
-    t.text "position", null: false
-    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
